4cdab506d0ea9b3d4c14adb6d9c421e884981b2d
Keifer Rasner <rasnerkeifer@gmail.com>
2022-04-19 19:15:24 -0500

    Structuring BlackJack Class Methods

--- assets/js/blackjackClass.js
+++ assets/js/blackjackClass.js
@@ -1,5 +1,46 @@
 class BlackJack {
-    constructor () {
-        
+    constructor (cards) {
+        this.cards = cards;
+        this.playerBalance = 1000;
+        this.playerBet = 25;
     }
+
+    // --- Game Manipulation Methods --- //
+    startGame () {}
+
+    endGame () {}
+
+    resetGame () {}
+
+    // --- Card Manipulation Methods --- //
+    dealCards () {}
+
+    shuffleDeck () {}
+
+    randomCardSelector () {}
+
+    createCard () {}
+
+    // --- Player Manipulated Event Methods --- //
+    addEvents () {}
+
+    hit () {}
+
+    stand () {}
+
+    double () {}
+
+    split () {}
+
+    bust () {}
+
+    // --- Scoring Methods --- //
+
+    updateScore () {}
+
+    // --- Changing Bet Methods --- //
+
+    increaseBet () {}
+
+    decreaseBet () {}
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-//
+const newGame = new BlackJack(cards);
+
+document.querySelector('.balance').innerText = `$${newGame.playerBalance}`;
+document.querySelector('.bet-amount').innerText = `$${newGame.playerBet}`;
+// Click Events
+newGame.addEvents();
